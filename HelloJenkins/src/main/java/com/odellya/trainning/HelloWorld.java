3fb74c9e60e176ec338af98e47d9a92f870cc3ab
odellyaconsulting <community@odellya.com>
2019-11-05 18:47:14 +0100

    premier commit du projet HelloWorld

---
+++ HelloJenkins/src/main/java/com/odellya/trainning/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.odellya.trainning;
+
+/**
+ * @author odellyaconsulting
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello World Jenkins");
+	}
+
+
+}
